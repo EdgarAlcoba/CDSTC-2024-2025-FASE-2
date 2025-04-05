0085b7125efab713601ce0d24e7ecd20b361d77a
Victor Martinez <vmartc06@estudiantes.unileon.es>
2025-04-05 19:00:33 +0200

    bugfix: fix date in /trips

--- Backend/API/dao/trips.py
+++ Backend/API/dao/trips.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from sqlalchemy import select
-import locale
 
 from ..dto.user import User
 from ..dto.trip import Trip
@@ -32,11 +31,46 @@ class Trips:
         for trip in trips:
             trip_data = trip.data
             trip_data["id"] = trip.id
-            default_locale = locale.getlocale(locale.LC_TIME)
-            locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
             weekday = trip.created_at.strftime("%A").capitalize()
             month_name = trip.created_at.strftime("%B").capitalize()
-            locale.setlocale(locale.LC_TIME, default_locale)
+            if weekday == "Monday":
+                weekday = "Lunes"
+            if weekday == "Tuesday":
+                weekday = "Martes"
+            if weekday == "Wednesday":
+                weekday = "Miércoles"
+            if weekday == "Thursday":
+                weekday = "Jueves"
+            if weekday == "Friday":
+                weekday = "Viernes"
+            if weekday == "Saturday":
+                weekday = "Sábado"
+            if weekday == "Sunday":
+                weekday = "Domingo"
+            if month_name == "January":
+                month_name = "Enero"
+            if month_name == "Februrary":
+                month_name = "Febrero"
+            if month_name == "March":
+                month_name = "Marzo"
+            if month_name == "April":
+                month_name = "Abril"
+            if month_name == "May":
+                month_name = "Mayo"
+            if month_name == "Junio":
+                month_name = "June"
+            if month_name == "July":
+                month_name = "Julio"
+            if month_name == "August":
+                month_name = "Agosto"
+            if month_name == "September":
+                month_name = "Septiembre"
+            if month_name == "October":
+                month_name = "Octubre"
+            if month_name == "November":
+                month_name = "Noviembre"
+            if month_name == "December":
+                month_name = "Diciembre"
             trip_data["generated_on"] = trip.created_at.strftime(f"{weekday} %-d de {month_name} de %Y")
 
             trips_return.append(
