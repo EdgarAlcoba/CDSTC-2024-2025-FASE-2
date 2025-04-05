91cfbedca93a606e9cb1371fc87aa7be85c864f7
Victor Martinez <vmartc06@estudiantes.unileon.es>
2025-04-05 18:51:08 +0200

    bugfix: change date format on /trips route

--- Backend/API/dao/trips.py
+++ Backend/API/dao/trips.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sqlalchemy import select
+import locale
 
 from ..dto.user import User
 from ..dto.trip import Trip
@@ -31,7 +32,13 @@ class Trips:
         for trip in trips:
             trip_data = trip.data
             trip_data["id"] = trip.id
-            trip_data["generated_on"] = trip.created_at.strftime("%Y-%m-%d %H:%M:%S")
+            default_locale = locale.getlocale(locale.LC_TIME)
+            locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
+            weekday = trip.created_at.strftime("%A").capitalize()
+            month_name = trip.created_at.strftime("%B").capitalize()
+            locale.setlocale(locale.LC_TIME, default_locale)
+            trip_data["generated_on"] = trip.created_at.strftime(f"{weekday} %-d de {month_name} de %Y")
+
             trips_return.append(
                 trip.data
             )
